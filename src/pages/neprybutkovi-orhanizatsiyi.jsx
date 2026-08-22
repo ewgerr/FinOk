5e36ae6223ee57898a0f9c1a652f63001b1cc560
ewger <eewger@gmail.com>
2026-08-22 03:23:03 +0300

    fix bug12

---
+++ src/pages/neprybutkovi-orhanizatsiyi.jsx
@@ -0,0 +1,6 @@
+import { serviceCategories } from "../lib/servicesData";
+import ServicePageTemplate from "../components/ServicePageTemplate";
+
+export default function NeprybutkoviOrhanizatsiyi() {
+  return <ServicePageTemplate category={serviceCategories[4]} />;
+}
