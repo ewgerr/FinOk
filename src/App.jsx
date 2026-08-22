fa98ed37ff2dc6fcd41205f5e2e2d6b0ab0f6cd0
ewger <eewger@gmail.com>
2026-08-22 03:37:39 +0300

    I undestend why page not found

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import TOV from './pages/TOV';
 import ITDiyaCity from './pages/ITDiyaCity';
 import GrantyNPO from './pages/GrantyNPO';
 import Konsaltyng from './pages/Konsaltyng';
+import NeprybutkoviOrhanizatsiyi from './pages/Neprybutkovi-orhanizatsiyi';
 import Price from './pages/Price';
 import About from './pages/About';
 import Contacts from './pages/Contacts';
